7377804b5cfa43b94e3955c03f7b058d7fd0e1b8
DrGorilla_md <dr_gorilla@protonmail.com>
2023-04-16 22:56:14 +0200

    thoughts

--- src/JBLite.sol
+++ src/JBLite.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -27,15 +27,21 @@ contract JBLite is ReentrancyGuard, Ownable {
     }
 
 // Todo: pack supported and balance:
-    mapping(uint256 projectId=>mapping(IERC20 tokenReceived=>bool supported)) public tokenSupportedBy;
+    mapping(uint256 projectId=>IERC20[] tokenReceived) public tokenSupportedBy;
+
+    mapping(IERC20 token=>address oracle) public oracleOf;
 
     mapping(uint256 projectId=>mapping(IERC20 tokenReceived=>uint256 balance)) public projectBalanceOf;
 
-    mapping(uint256 projectId=>Splits[] splitsOf) public splitsOf;
+    mapping(uint256 projectId=>Splits[] splitsOf) public splitsOf; // Locked during fc lock
 
     mapping(uint256 projectId=>FundingCycle fundingCycle) public fundingCycleOf;
 
+
     mapping(uint256 projectId=>IERC20 projectToken) public projectTokenOf;
+
+    mapping(uint256 projectId=>IERC721 NFT) public NFTOf;
+
 
 // todo: eth -> weth only, add a wrapper 
     function pay(uint256 _projectId, IERC20 _paymentToken, uint256 _amount)
@@ -43,11 +49,11 @@ contract JBLite is ReentrancyGuard, Ownable {
         nonReentrant
         returns (uint256 _projectTokenReceived)
     {
-        if (!tokenSupportedBy[_projectId][_paymentToken]) revert JBLite_UnsupportedTokenReceived();
+        if (oracleOf[_paymentToken] == address(0)) revert JBLite_UnsupportedTokenReceived();
         _paymentToken.transferFrom(msg.sender, address(this), _amount);
 
         // add a check for fee on transfer?
-
+    
         // oraclize the amount -> if oracle == address(0), token is unsupported at protocol level
         uint256 _amountInDollars; // todo 
 
@@ -61,11 +67,15 @@ contract JBLite is ReentrancyGuard, Ownable {
         // mint if non-0, from projectTokenOf
 
         // if erc721 -> mint
-        // todo: tiering
-        uint256 _nftToMint = _amountInDollars * _currentFundingCycle.nftWeight;
+        // todo: tiering -> in NFT contract
+        IERC721 projectNFT = NFTOf[_projectId];
+
         // mint
 
-        if(_amountToMint * _nftToMint == 0) revert JBLite_ProjectPaymentsPaused();
+        // Both 0?
+        if(_amountToMint + uint256(uint160(address(projectNFT))) == 0) revert JBLite_ProjectPaymentsPaused();
+
+        _projectTokenReceived;
     }
 
     function redeem(uint256 _projectId, uint256 _projectTokenAmount, IERC20 _tokenToRedeem)
@@ -77,7 +87,10 @@ contract JBLite is ReentrancyGuard, Ownable {
 
         // burn
 
-        // compute amount redeemed, if 0, revert
+        // compute amount redeemed, if 0, revert:
+        // compute project total value (loop over project token, cumsum quote in USD)
+        // get the amount to redeem in usd (quote)
+        // check if enough of the token out to cover it
 
         // send token out
     }
@@ -91,6 +104,9 @@ contract JBLite is ReentrancyGuard, Ownable {
     }
 
     function setProjectTokenSupport() external {}
+    
+    function addNFT() external {}
 
     function modifyOracle() external onlyOwner {}
+
 }
